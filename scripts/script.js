038dfee7aa8f5d5eebad4016be6d24cf71b77346
NatalieSn <senuskinanatalia@gmail.com>
2024-12-04 16:47:08 +0000

    js слайдер

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+
+    pagination: {
+        el: '.swiper-pagination',
+      },
+});
