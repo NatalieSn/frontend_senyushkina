ec063b9a45811953c66a33ba8aad95d4a49a19dc
NatalieSn <senuskinanatalia@gmail.com>
2024-12-11 16:50:38 +0000

    стилизация+js

---
+++ scripts/reviews.js
@@ -0,0 +1,29 @@
+const facilities = {
+    facilities_1: {
+        text: 'I can always find what Im looking for in Splice, whether its the exact sound I want or just a bit of inspiration.',
+        name: 'Andrew Huang',
+        artist: 'Artist',
+        avatar: '../img/review1.png',
+    },
+    
+    facilities_2: {
+        text: 'Finally a way to buy plugins that works. By paying a little at a time, producers can get legit access to the top VSTs.',
+        name: 'KSHMR',
+        artist: 'Artist',
+        avatar: '../img/review2.png',
+    },
+
+    facilities_3: {
+        text: 'Its been fun to dive into Splicers creator community and explore tools that support my own creative process.',
+        name: 'Kesha Lee',
+        artist: 'Artist',
+        avatar: '../img/review3.png',
+    },
+
+    facilities_4: {
+        text: 'I can always find what Im looking for in Splice, whether its the exact sound I want or just a bit of inspiration.',
+        name: 'Andrew Huang',
+        artist: 'Artist',
+        avatar: '../img/review1.png',
+    },
+}
