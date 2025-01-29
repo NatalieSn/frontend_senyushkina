64602cbe968794399ed37cf8ebe995d0bacb3050
Наталия Сенюшкина <senuskinanatalia@gmail.com>
2025-01-29 19:37:09 +0300

    Задание 2.2 Форма

--- scripts/script.js
+++ scripts/script.js
@@ -7,3 +7,38 @@ const swiper = new Swiper('.swiper', {
         el: '.swiper-pagination',
       },
 });
+
+
+const modal = document.getElementById("myModal");
+  const btn = document.getElementById("openModal");
+  const span = document.getElementsByClassName("modal__close")[0];
+  const okButton = document.getElementById("okButton");
+  const cancelButton = document.getElementById("cancelButton");
+
+  btn.onclick = function() {
+      modal.style.display = "block";
+      document.body.style.overflow = "hidden"; // Отключаем скролл
+  }
+
+  span.onclick = function() {
+      closeModal();
+  }
+
+  okButton.onclick = function() {
+      closeModal();
+  }
+
+  cancelButton.onclick = function() {
+      closeModal();
+  }
+
+  window.onclick = function(event) {
+      if (event.target == modal) {
+          closeModal();
+      }
+  }
+
+  function closeModal() {
+      modal.style.display = "none";
+      document.body.style.overflow = "auto"; // Включаем скролл
+  }
