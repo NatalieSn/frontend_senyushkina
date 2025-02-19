963ebd54675ffe024849eaba51625de935b8dd82
Наталия Сенюшкина <senuskinanatalia@gmail.com>
2025-02-19 19:51:32 +0300

    Задание 2. Карточки

--- scripts/script.js
+++ scripts/script.js
@@ -56,58 +56,71 @@ const modal = document.getElementById("myModal");
 
 const cards = {
     card1: {
-        text: "I can always find what Im looking for in Splice, whether its the exact sound I want or just a bit of inspiration.",
-        img: "../img/review1.png",
+        numb: "1",
+        text: "I can always find what I'm looking for in Splice, whether it's the exact sound I want or just a bit of inspiration.",
+        img: "img/review1.png",
         author: "Andrew Huang",
         artist: "Artist",
     },
     card2: {
+        numb: "2",
         text: "Finally a way to buy plugins that works. By paying a little at a time, producers can get legit access to the top VSTs.",
-        img: "../img/review2.png",
+        img: "img/review2.png",
         author: "KSHMR",
         artist: "Artist",
     },
     card3: {
-        text: "Its been fun to dive into Splicers creator community and explore tools that support my own creative process.",
-        img: "../img/review3.png",
+        numb: "3",
+        text: "It's been fun to dive into Splicer's creator community and explore tools that support my own creative process.",
+        img: "img/review3.png",
         author: "Kesha Lee",
         artist: "Artist",
     },
     card4: {
-        text: "I can always find what Im looking for in Splice, whether its the exact sound I want or just a bit of inspiration.",
-        img: "../img/review1.png",
+        numb: "4",
+        text: "I can always find what I'm looking for in Splice, whether it's the exact sound I want or just a bit of inspiration.",
+        img: "img/review1.png",
         author: "Andrew Huang",
         artist: "Artist",
     },
 };
 
-function showCard(cards) {
-    let reviewCards = document.getElementById("cards");
-    console.log(reviewCards);
-    let newCard = `
-        <div class="cards__review-2">
-            <p class="review__text">${cards.text}</p>
+function showCard(card) {
+    return `
+        <div class="cards__review-${card.numb}">
+            <p class="review__text">${card.text}</p>
             <div class="review__author">
-                <img src="${cards.img}" class="review__img">
+                <img src="${card.img}" class="review__img">
                 <div class="author">
-                    <p class="author__name">${cards.author}</p>
-                    <p class="author__artist">${cards.artist}</p>
+                    <p class="author__name">${card.author}</p>
+                    <p class="author__artist">${card.artist}</p>
                 </div>
             </div>
         </div>
     `;
-    reviewCards.innerHTML += newCard;
 }
 
 function showAllCards(allCards) {
-	for (let i = 0; i < allCards.length; i++) {
-		showCard(allCards[i]);
-	}
+    const reviewCards = document.getElementById("cards");
+    if (!reviewCards) {
+        console.error("Element with id 'cards' not found.");
+        return;
+    }
+
+    // Очистить существующие карточки
+    reviewCards.innerHTML = '';
+
+    // Добавить все карточки
+    for (let key in allCards) {
+        reviewCards.insertAdjacentHTML("beforeend", showCard(allCards[key]));
+    }
 }
 
 showAllCards(cards);
 
 function clearCards() {
-	let reviewCards = document.getElementById("cards");
-	reviewCards.innerHTML = "";
+    const reviewCards = document.getElementById("cards");
+    if (reviewCards) {
+        reviewCards.innerHTML = "";
+    }
 }
